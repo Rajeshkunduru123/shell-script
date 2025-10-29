55361846e0cdbe35073656daa16e2735741f38d1
rajesh <rajeshkunduru123.com>
2025-10-29 20:07:22 +0530

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "pleasse enter username::"
+
+read username #here username is variable
+
+echo"please enter password::"
+
+read password
+
+echo "username is: $username, password is: $password"
