5c6c5e08aebed5f0c358b9ee0f6aab9e4bf677e9
rajesh <rajeshkunduru123.com>
2025-10-29 13:54:14 +0530

    hello script

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh:: I am fine too. How is your worl?"
+echo "Suresh:: not bad. I am thinking toupgrade to Devops"
+
