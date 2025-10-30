000bf20ad0322669be1135e839331e572f7d6cf2
rajesh <rajeshkunduru123.com>
2025-10-30 16:28:10 +0530

    hello script

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES("RRR" "DJ" "RR")
+
+# size of above array is 3.
+# index are 0,1,2
+
+# list always starts with 0.
+
+echo "FIrst Movie is: ${MOVIES[0]}"
+echo "FIrst Movie is: ${MOVIES[1]}"
+echo "FIrst Movie is: ${MOVIES[0]}"
