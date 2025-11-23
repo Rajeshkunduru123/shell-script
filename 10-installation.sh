1e2abb2635631e5b7628e00ec6c024c529f1b662
rajesh <rajeshkunduru123.com>
2025-11-23 10:00:36 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -13,7 +13,16 @@ fi
 
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql...FAILURE"
+    exit 1
+
+fi
+
+dnf install mysql -y
+
 echo "is script proceeding?"
 
 
-dnf install mysql -y
+dnf install git -y
